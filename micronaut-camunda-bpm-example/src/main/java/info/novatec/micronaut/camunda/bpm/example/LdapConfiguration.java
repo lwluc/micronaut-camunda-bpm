30f7b52d6d4981199f68aad6df0d69a7f956ebae
Luc Weinbrecht <luc.weinbrecht@novatec-gmbh.de>
2021-02-19 08:54:39 +0100

    added LdapIdentityProviderPlugin

---
+++ micronaut-camunda-bpm-example/src/main/java/info/novatec/micronaut/camunda/bpm/example/LdapConfiguration.java
@@ -0,0 +1,35 @@
+package info.novatec.micronaut.camunda.bpm.example;
+
+import info.novatec.micronaut.camunda.bpm.feature.DefaultProcessEngineConfigurationCustomizer;
+import info.novatec.micronaut.camunda.bpm.feature.MnProcessEngineConfiguration;
+import info.novatec.micronaut.camunda.bpm.feature.ProcessEngineConfigurationCustomizer;
+import io.micronaut.context.annotation.Replaces;
+import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
+import org.camunda.bpm.identity.impl.ldap.plugin.LdapIdentityProviderPlugin;
+
+import javax.inject.Singleton;
+import java.util.List;
+
+@Singleton
+@Replaces(DefaultProcessEngineConfigurationCustomizer.class)
+public class LdapConfiguration implements ProcessEngineConfigurationCustomizer {
+
+    @Override
+    public void customize(MnProcessEngineConfiguration processEngineConfiguration) {
+        List<ProcessEnginePlugin> plugins = processEngineConfiguration.getProcessEnginePlugins();
+        plugins.add(ldapIdentityProvider());
+        processEngineConfiguration.setProcessEnginePlugins(plugins);
+    }
+
+    // Using an open online LDAP to provide an example
+    // https://www.forumsys.com/tutorials/integration-how-to/ldap/online-ldap-test-server/
+    // Log in e.g. with einstein + password
+    private LdapIdentityProviderPlugin ldapIdentityProvider() {
+        LdapIdentityProviderPlugin ldap = new LdapIdentityProviderPlugin();
+        ldap.setServerUrl("ldap://ldap.forumsys.com:389");
+        ldap.setManagerDn("cn=read-only-admin,dc=example,dc=com");
+        ldap.setManagerPassword("password");
+        ldap.setBaseDn("dc=example,dc=com");
+        return ldap;
+    }
+}
